507a0f0fd926118f912622958c379e1efc136533
Monica-pxl <monica.munozdelatorre@cieep.com>
2026-06-30 23:04:38 +0200

    Base de datos hecha con su estrcutura de tablas

---
+++ backend/prisma/migrations/20260630205557_init/migration.sql
@@ -0,0 +1,128 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `nickname` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NULL,
+    `estado` ENUM('en_linea', 'desconectado') NOT NULL,
+    `rol` ENUM('user', 'admin') NOT NULL,
+    `estado_cuenta` ENUM('activa', 'suspendida', 'baneada') NOT NULL,
+    `fecha_creacion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `ultima_conexion` DATETIME(3) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_nickname_key`(`nickname`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Sala` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tipo` ENUM('general_anual', 'epoca_estilo') NOT NULL,
+    `ano` INTEGER NULL,
+    `epocaId` INTEGER NULL,
+    `tematicaId` INTEGER NULL,
+    `nombre` VARCHAR(191) NOT NULL,
+    `descripcion` VARCHAR(191) NULL,
+    `cerrada` BOOLEAN NOT NULL DEFAULT false,
+    `fecha_creacion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Epoca` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Tematica` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+    `descripcion` VARCHAR(191) NULL,
+    `epocaId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MensajeSala` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `contenido` VARCHAR(191) NOT NULL,
+    `tipo` ENUM('texto', 'imagen', 'sticker', 'audio') NOT NULL,
+    `fecha_creacion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+    `salaId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ChatPrivado` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `usuario1Id` INTEGER NOT NULL,
+    `usuario2Id` INTEGER NOT NULL,
+    `fecha_creacion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MensajePrivado` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `contenido` VARCHAR(191) NOT NULL,
+    `tipo` ENUM('texto', 'imagen', 'sticker', 'audio') NOT NULL,
+    `fecha_creacion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `chatId` INTEGER NOT NULL,
+    `emisorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Amistad` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `emisorId` INTEGER NOT NULL,
+    `receptorId` INTEGER NOT NULL,
+    `estado` ENUM('pendiente', 'aceptado', 'rechazado') NOT NULL,
+    `fecha_creacion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Sala` ADD CONSTRAINT `Sala_epocaId_fkey` FOREIGN KEY (`epocaId`) REFERENCES `Epoca`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Sala` ADD CONSTRAINT `Sala_tematicaId_fkey` FOREIGN KEY (`tematicaId`) REFERENCES `Tematica`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Tematica` ADD CONSTRAINT `Tematica_epocaId_fkey` FOREIGN KEY (`epocaId`) REFERENCES `Epoca`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MensajeSala` ADD CONSTRAINT `MensajeSala_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MensajeSala` ADD CONSTRAINT `MensajeSala_salaId_fkey` FOREIGN KEY (`salaId`) REFERENCES `Sala`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ChatPrivado` ADD CONSTRAINT `ChatPrivado_usuario1Id_fkey` FOREIGN KEY (`usuario1Id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ChatPrivado` ADD CONSTRAINT `ChatPrivado_usuario2Id_fkey` FOREIGN KEY (`usuario2Id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MensajePrivado` ADD CONSTRAINT `MensajePrivado_chatId_fkey` FOREIGN KEY (`chatId`) REFERENCES `ChatPrivado`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MensajePrivado` ADD CONSTRAINT `MensajePrivado_emisorId_fkey` FOREIGN KEY (`emisorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Amistad` ADD CONSTRAINT `Amistad_emisorId_fkey` FOREIGN KEY (`emisorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Amistad` ADD CONSTRAINT `Amistad_receptorId_fkey` FOREIGN KEY (`receptorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
